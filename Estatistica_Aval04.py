97584c86ab0a4b5b9592f7127dba7a750b221f85
MarcosAbdalla82 <009113638@senaimgdocente.com.br>
2026-02-04 20:36:50 -0300

    Update Estatistica_Aval04.py

--- Estatistica_Aval04.py
+++ Estatistica_Aval04.py
@@ -116,8 +116,8 @@ with col1:
 with col2:
     data_fim = st.date_input("Data final", value=datetime.today().date())
 
-inicio_iso = datetime.combine(data_inicio, time.min)
-fim_iso = datetime.combine(data_fim, time.max)
+inicio_iso = inicio.strftime("%Y-%m-%d %H:%M")
+fim_iso = fim.strftime("%Y-%m-%d %H:%M")
 
 # ================= ABA 1 ================= #
 
@@ -239,6 +239,7 @@ with aba5:
     st.metric("ISCF", f"{ISCF:.2f}%")
     st.metric("ISCS", f"{ISCS:.2f}%")
     st.metric("PLP", f"{PLP:.2f}%")
+
 
 
 
